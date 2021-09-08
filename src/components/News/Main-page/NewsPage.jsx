5900a6efa7b5479512cafe45007565d687111e47
Mateen Mirani <mateenmirani125@gmail.com>
2021-09-08 10:56:42 +0500

    PakNews update code with infinite scroll bar

--- src/components/News/Main-page/NewsPage.jsx
+++ src/components/News/Main-page/NewsPage.jsx
@@ -1,21 +1,24 @@
 import React, { useState, useEffect } from "react";
 import NewsItem from "../NewsItem/NewsItem";
 import Spinner from "../Spinner/Spinner";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const NewsPage = (props) => {
   let articles = [];
 
-  const [enterdNews, setEnterdNews] = useState(articles);
+  const [enteredNews, setEnteredNews] = useState(articles);
+  let [totalRes, setTotalRes] = useState(0);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
 
   const fetchData = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=e6146df6a08249b196a409b6d30e70aa&page=${page}&pageSize=${props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=e6146df6a08249b196a409b6d30e70aa&page=${page}&pageSize=${props.pageSize}`;
     setLoading(true);
     try {
       let data = await fetch(url);
       let parsedData = await data.json();
-      setEnterdNews((articles = parsedData.articles));
+      setEnteredNews((articles = parsedData.articles));
+      setTotalRes((totalRes = parsedData.totalRes));
       setLoading(false);
     } catch (error) {
       console.log(error);
@@ -26,41 +29,15 @@ const NewsPage = (props) => {
     fetchData();
   }, []);
 
-  const nextPageHandler = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=${
-      props.country
-    }&category=${props.category}&apiKey=e6146df6a08249b196a409b6d30e70aa&page=${
-      page + 1
-    }&pageSize=${props.pageSize}`;
-
-    setLoading(true);
+  const fetchMoreData = async () => {
+    setPage(page + 1);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=e6146df6a08249b196a409b6d30e70aa&page=${page}&pageSize=${props.pageSize}`;
 
     try {
       let data = await fetch(url);
       let parsedData = await data.json();
-      setEnterdNews((articles = parsedData.articles));
-      setPage(page + 1);
-      setLoading(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const prevPageHandler = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=${
-      props.country
-    }&category=${props.category}&apiKey=e6146df6a08249b196a409b6d30e70aa&page=${
-      page - 1
-    }&pageSize=${props.pageSize}`;
-
-    setLoading(true);
-
-    try {
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      setEnterdNews((articles = parsedData.articles));
-      setPage(page - 1);
-      setLoading(false);
+      setEnteredNews(enteredNews.concat(parsedData.articles));
+      setTotalRes((totalRes = parsedData.totalRes));
     } catch (error) {
       console.log(error);
     }
@@ -72,47 +49,35 @@ const NewsPage = (props) => {
         PakNews - Top Headlines
       </h1>
       {loading && <Spinner />}
-      <div className="container my-4">
-        <div className="row">
-          {enterdNews.map((items, index) => {
-            return (
-              <div className="col-md-4 my-2" key={index}>
-                <NewsItem
-                  title={items.title}
-                  description={items.description}
-                  imgUrl={
-                    !items.urlToImage
-                      ? "https://dailyutahchronicle.com/wp-content/uploads/2021/09/JG_20210829_S1_15.jpg"
-                      : items.urlToImage
-                  }
-                  auther={items.author ? items.author : "Unknown"}
-                  date={new Date(items.publishedAt).toLocaleDateString()}
-                  newsUrl={items.url}
-                />
-              </div>
-            );
-          })}
+      <InfiniteScroll
+        dataLength={enteredNews.length}
+        next={fetchMoreData}
+        hasMore={enteredNews.length !== totalRes}
+        loader={<Spinner />}
+      >
+        <div className="container my-4">
+          <div className="row">
+            {enteredNews.map((items, index) => {
+              return (
+                <div className="col-md-4 my-2" key={index}>
+                  <NewsItem
+                    title={items.title}
+                    description={items.description}
+                    imgUrl={
+                      !items.urlToImage
+                        ? "https://dailyutahchronicle.com/wp-content/uploads/2021/09/JG_20210829_S1_15.jpg"
+                        : items.urlToImage
+                    }
+                    auther={items.author ? items.author : "Unknown"}
+                    date={new Date(items.publishedAt).toLocaleDateString()}
+                    newsUrl={items.url}
+                  />
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
-
-      <div className="container my-4 d-flex justify-content-between">
-        <button
-          disabled={page <= 1}
-          type="button"
-          className="btn btn-danger btn-sm"
-          onClick={prevPageHandler}
-        >
-          <i className="fas fa-chevron-left" />
-          Previous
-        </button>
-        <button
-          type="button"
-          className="btn btn-success"
-          onClick={nextPageHandler}
-        >
-          Next <i className="fas fa-chevron-right" />
-        </button>
-      </div>
+      </InfiniteScroll>
     </>
   );
 };
--- src/components/News/Spinner/Spinner.jsx
+++ src/components/News/Spinner/Spinner.jsx
@@ -3,7 +3,7 @@ import Loading from "./loading.gif";
 
 const Spinner = () => {
   return (
-    <div className="text-center">
+    <div className="text-center my-3">
       <img src={Loading} alt="Spinner Loading" />
     </div>
   );
